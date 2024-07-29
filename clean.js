d9b6730aa7d1f203f6a211076ec8e8c1fc5efc49
longnt08 <thanhlong0j@gmail.com>
2024-07-30 00:30:45 +0700

    update

--- clean.js
+++ clean.js
@@ -7,10 +7,6 @@ function cleanHTML(html) {
   
     html = html.replace(/<(\w+)([^>]*)>\s*<\/\1>/g, '');
   
-    html = html.replace(/<(\w+)([^>]*)\s(class|style|data-[^=]+)="[^"]*"\s?([^>]*)>/g, '<$1$2 $4>');
-  
-    html = html.replace(/<(script|style)[^>]*>[\s\S]*?<\/\1>/g, '');
-  
     return html;
 }
 
