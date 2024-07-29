8c0cfaf1f1897b240cf99d383f720caa1830cffe
longnt08 <thanhlong0j@gmail.com>
2024-07-29 21:34:58 +0700

    update compress.js & popup.html

--- compress.js
+++ compress.js
@@ -2,7 +2,7 @@ import { cleanHTML } from "./clean.js";
 
 function gzipCompressHTML(html) {
     const cleaned_html = cleanHTML(html);
-    const compressed_html = pako.gzip(html, {to: 'string'});
+    const compressed_html = pako.gzip(cleaned_html, {to: 'string'});
     return compressed_html;
 }
 
